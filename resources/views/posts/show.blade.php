4bf5b17ca444b51b07a11af86bdfc56a1fea9067
RileyDMK <rileydmathis@gmail.com>
2017-09-28 10:51:12 -0700

    Bug fix: Update form method

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,7 +18,7 @@
   <hr />
   <div class="card">
     <div class="card-block">
-      <form method="POST" actions="/posts/{{ $post->id }}/comments">
+      <form method="POST" action="/posts/{{ $post->id }}/comments">
         {{ csrf_field() }}
         <div class="form-group">
           <textarea name="body" placeholder="Your comment here." class="form-control">
